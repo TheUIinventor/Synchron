3efd0032e8db547003cf44d9e301732e4654e798
Nadisha <nadisha.widanagamagedon@gmail.com>
2025-08-13 10:37:31 +1000

    tried to solve homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 
-
 // app/page.tsx (Server Component)
 import dynamic from "next/dynamic";
 
 const HomeClient = dynamic(() => import("./home-client"), { ssr: false });
 
 export default function Home() {
-  return <HomeClient />;
+  return (
+    <div className="container max-w-6xl mx-auto px-4 py-6">
+      <div className="flex flex-col md:flex-row gap-6">
+        {/* Sidebar placeholder for desktop (navigation is handled globally) */}
+        <div className="hidden md:block w-64 flex-shrink-0"></div>
+        {/* Main content */}
+        <div className="flex-1">
+          <HomeClient />
+        </div>
+      </div>
+    </div>
+  );
 }
