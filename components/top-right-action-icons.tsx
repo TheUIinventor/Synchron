e3fc116b09510c0ccc12dc1dd6dd19d7c7f42c0f
Nadisha <nadisha.widanagamagedon@gmail.com>
2025-10-15 21:08:07 +1100

    tried to remove double buttons

--- components/top-right-action-icons.tsx
+++ components/top-right-action-icons.tsx
@@ -3,12 +3,5 @@ import { AuthButton } from "@/components/auth-button";
 import SettingsMenu from "@/components/settings-menu";
 import ThemeToggle from "@/components/theme-toggle";
 
-export default function TopRightActionIcons() {
-  return (
-    <div className="absolute top-6 right-8 z-40 flex gap-2">
-      <AuthButton />
-      <SettingsMenu />
-      <ThemeToggle />
-    </div>
-  );
+  return null;
 }
